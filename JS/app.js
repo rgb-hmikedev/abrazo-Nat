242c8429e7e2bbb8fbf637e6511cbfd57474a789
rgb-hmikedev <hmontemayorrios2@gmail.com>
2023-11-05 10:20:55 -0600

    primer commit

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+
+
+setTimeout(cambiaMensaje, 2300)
+
+function cambiaMensaje(){
+ document.getElementById("loading-message").innerText = "Enviado!"
+}
+
+setTimeout(function (){
+    alert("Recibiste un abrazo de Babu!")
+
+}, 2400)
+
+
+
+
